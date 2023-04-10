bfaef7c7623e10366f5983b890635da544dcffcb
Konstantin Loktev <konstantin.loktev.02@mail.ru>
2023-04-10 17:10:19 +0300

    stat2 rabotaet vrode

--- statoiv/lab2/lab2.py
+++ statoiv/lab2/lab2.py
@@ -10,51 +10,75 @@ from sklearn.pipeline import make_pipeline
 def nanToZeroMapper(arg):
     return 0 if math.isnan(arg) else arg
 
-PATH = "E:/Important/uchoba_rep/uchoba/statoiv/lab2/car details v4.csv"
+PATH = "E:/Important/uchoba_rep/uchoba/statoiv/lab2/weatherHistory.csv"
 DATASET_SIZE = 5000
 dataset = pd.read_csv(PATH)
 # dataset = dataset.head(DATASET_SIZE)
 UNNECESSARY_COLUMNS = [
-    'Make',
-    'Model',
-    'Year',
-    'Fuel Type',
-    'Transmission',
-    'Location',
-    'Color',
-    'Owner',
-    'Seller Type',
-    'Engine',
-    'Max Torque',
-    'Drivetrain',
-    'Length',
-    'Width',
-    'Height',
-    'Seating Capacity',
-    'Fuel Tank Capacity',
-    'Kilometer'
+    'Formatted Date',
+    'Summary',
+    'Precip Type',
+    'Temperature (C)',
+    'Apparent Temperature (C)',
+    'Wind Speed (km/h)',
+    'Wind Bearing (degrees)',
+    'Loud Cover',
+    'Pressure (millibars)',
+    'Daily Summary'
 ]
-FIRST_REGRESSION_COLUMN = 'Price'
-SECOND_REGRESSION_COLUMN = 'Max Power'
+FIRST_REGRESSION_COLUMN = 'Humidity'
+SECOND_REGRESSION_COLUMN = 'Visibility (km)'
 dataset = dataset.drop(UNNECESSARY_COLUMNS, axis=1)
-dataset[SECOND_REGRESSION_COLUMN] = dataset[SECOND_REGRESSION_COLUMN].map(lambda hp: float(str(hp).split('@')[0].split(' ')[0])).map(nanToZeroMapper)
-
-# sns.set_style('darkgrid')
-# sns.set_palette('Set2')
-# sns.lineplot(
-#     x=dataset[FIRST_REGRESSION_COLUMN],
-#     y=dataset[SECOND_REGRESSION_COLUMN],
-# )
-# plt.title('График зависимости')
-# plt.xlabel('Цена')
-# plt.ylabel('Максимальная мощность (л.с.)')
-# plt.show()
+dataset[FIRST_REGRESSION_COLUMN] = dataset[FIRST_REGRESSION_COLUMN].map(lambda x: x * 100)
 
 x = np.array(dataset[FIRST_REGRESSION_COLUMN]).reshape(-1, 1)
 y = np.array(dataset[SECOND_REGRESSION_COLUMN]).reshape(-1, 1)
-DEGREES = 3
+DEGREES = 6
 regression = make_pipeline(PolynomialFeatures(DEGREES), LinearRegression())
 regression.fit(x, y)
 predictions = regression.predict(x)
 mean_squared_error = np.mean((predictions - np.array(y)) ** 2)
-print(f'Среднеквадратическая ошибка = {mean_squared_error}')
+
+coefs = list(np.append(
+    regression['linearregression'].intercept_[0],
+    regression['linearregression'].coef_[0][1:]
+))
+
+START_ROUNDING_COEFS_FROM = 2
+equation = ''
+for index, coef in enumerate(coefs):
+    curr_decimal_places_in_coef = START_ROUNDING_COEFS_FROM
+    rounded_coef = round(coef, curr_decimal_places_in_coef)
+    while(round(coef, curr_decimal_places_in_coef) == 0):
+        curr_decimal_places_in_coef += 1
+        rounded_coef = round(coef, curr_decimal_places_in_coef)
+
+    if index == 0:
+        equation += f'{rounded_coef} '
+        continue
+    positive = rounded_coef >= 0
+    rounded_coef = abs(rounded_coef)
+    power = '' if index == 1 else f'^{index}'
+    equation += f"{'+' if positive else '-'} {rounded_coef}x{power} " 
+
+print(f'Уравнение регрессии: {equation}')
+print(f'Среднеквадратическая ошибка: {mean_squared_error}')
+
+sns.lineplot(
+    x=dataset[FIRST_REGRESSION_COLUMN],
+    y=dataset[SECOND_REGRESSION_COLUMN],
+    linestyle="solid"
+)
+sns.lineplot(
+    x=dataset[FIRST_REGRESSION_COLUMN],
+    y=predictions.reshape(-1),
+    linestyle="dotted"
+)
+plt.title(
+    'График зависимости\n\n'
+    'Сплошная линия - эталонные значения\n'
+    'Точечная линия - предсказания регрессии'
+)
+plt.xlabel('Влажность, %')
+plt.ylabel('Видимость, км')
+plt.show()
