769dc2aaec3817a78b521339fd7d8834d4a56c09
Konstantin Loktev <konstantin.loktev.02@mail.ru>
2023-04-12 22:46:26 +0300

    seivim predlozhynki

--- eyaziis/lab2/front/script.js
+++ eyaziis/lab2/front/script.js
@@ -1,4 +1,8 @@
-const fileReaderModule = (function () {
+const inputSentenceContainer = document.getElementById('input-sentence');
+const decomposedSentenceContainer = document.getElementById('decomposed-sentence');
+const buttonBlock = document.getElementById('button-block');
+
+const fileModule = (function () {
     async function readTextFile(file) {
         return new Promise((resolve, reject) => {
             const reader = new FileReader();
@@ -9,13 +13,28 @@ const fileReaderModule = (function () {
         });
     }
 
+    function download(text, filename) {
+        const file = new Blob([text]);
+        const a = document.createElement("a");
+        const url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function () {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);
+        }, 0); 
+    }
+
     return {
-        readTextFile
+        readTextFile,
+        download
     }
 })();
 
 document.getElementById('sentence-input').addEventListener('change', async function () {
-    const sentence = await fileReaderModule.readTextFile(this.files[0]);
+    const sentence = await fileModule.readTextFile(this.files[0]);
     const decomposed_sentence = await fetch(`http://localhost:5000/decompose`, {
         method: 'POST',
         headers: {
@@ -26,6 +45,14 @@ document.getElementById('sentence-input').addEventListener('change', async funct
     .then(response => response.json())
     .then(response => response.result)
     .catch(err => console.error(err));
-    console.log(sentence);
-    console.log(decomposed_sentence);
+    inputSentenceContainer.innerHTML = sentence;
+    decomposedSentenceContainer.innerHTML = decomposed_sentence;
+    buttonBlock.style.opacity = 1;
+});
+
+document.getElementById('save').addEventListener('click', () => {
+    fileModule.download(
+        decomposedSentenceContainer.innerHTML,
+        'decomposition.txt'
+    )
 });
