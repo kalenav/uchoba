72904afce975ccbb64edf1d802e688f33804f772
Konstantin Loktev <konstantin.loktev.02@mail.ru>
2023-05-16 09:53:35 +0300

    transfer

--- lois/sem6_lab3/lab3_start_state.pl
+++ lois/sem6_lab3/lab3_start_state.pl
@@ -1,36 +1,48 @@
-beats(queen(XF, YF), cell(X, Y)) :-
+beats(queen(XF, YF), cell(X, Y)):-
     \+ (X == XF, Y == YF),
     (X == XF; Y == YF; X-Y =:= XF-YF; X+Y =:= XF+YF).
 
 beaten(Cell, Queens):-
     member(Queen, Queens),
     beats(Queen, Cell).
-
-beaten(Cell, Queens, IgnoredQueen):-
+beaten(cell(X, Y), Queens, ignored_queen(XF, YF)):-
+    \+ (X == XF, Y == YF),
     member(Queen, Queens),
-    Queen \= IgnoredQueen,
-    beats(Queen, Cell).
+    beats(Queen, cell(X, Y)).
+
+occupied(cell(X, Y), Queens):-
+    member(queen(XF, YF), Queens),
+    X == XF,
+    Y == YF,
+    write(queen(XF, YF)), nl.
 
 no_queens_beat_each_other([], _).
 no_queens_beat_each_other([queen(XF, YF)|RemainingQueens], Queens):-
     \+ beaten(cell(XF, YF), Queens),
     no_queens_beat_each_other(RemainingQueens, Queens).
 
-move_queen_to_non_beaten_cell_and_update_new_queen_list(queen(XF, YF), Queens, Result):-
-    beaten(cell(XF, YF), Queens),
-    RowsAndColumns = [1, 2, 3, 4, 5, 6, 7, 8],
-    member(X, RowsAndColumns),
-    member(Y, RowsAndColumns),
-    \+ beaten(cell(X, Y), Queens, queen(XF, YF)),
-    # write(queen(X, Y)).
-
-solve(StartState, Result):-
-    (no_queens_beat_each_other(StartState, StartState) ->
-        Result = StartState
+move_queen_to_non_beaten_cell_and_update_state(queen(XF, YF), CurrState, NewState):-
+    (beaten(cell(XF, YF), CurrState) ->
+        RowsAndColumns = [1, 2, 3, 4, 5, 6, 7, 8],
+        member(X, RowsAndColumns),
+        member(Y, RowsAndColumns),
+        \+ beaten(cell(X, Y), CurrState, queen(XF, YF)),
+        \+ occupied(cell(X, Y), CurrState),
+        select(queen(XF, YF), CurrState, queen(X, Y), NewState) 
     ;
-        # move_queens_to_non_beaten_cells(StartState, StartState, Result)
+        NewState = CurrState
     ).
 
-solve_and_write(State):-
-    solve(State, Result),
+solve(CurrState, QueensMoved, Result):-
+    (no_queens_beat_each_other(CurrState, CurrState); QueensMoved == 8 ->
+        Result = CurrState
+    ;
+        NewQueensMoved is QueensMoved + 1,
+        nth1(NewQueensMoved, CurrState, Queen),
+        move_queen_to_non_beaten_cell_and_update_state(Queen, CurrState, NewState),
+        solve(NewState, NewQueensMoved, Result)
+    ).
+
+solve_and_write(StartState):-
+    solve(StartState, 0, Result),
     write(Result).
