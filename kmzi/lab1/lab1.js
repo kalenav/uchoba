ffc6a1f17ca7eb0bd8f512bda2082ead4028a852
Konstantin Loktev <konstantin.loktev.02@mail.ru>
2023-09-07 09:56:33 +0300

    kmzi1

--- kmzi/lab1/lab1.js
+++ kmzi/lab1/lab1.js
@@ -5,5 +5,18 @@ function generateAndDownloadFile(numberArray) {
     element.click();
 }
 
-generateAndDownloadFile(new Array(1024).fill(0).map(Math.random));
-generateAndDownloadFile(window.crypto.getRandomValues(new Uint8Array(1024)));
+// generateAndDownloadFile(new Array(1024).fill(0).map(Math.random));
+// generateAndDownloadFile(window.crypto.getRandomValues(new Uint8Array(1024)));
+
+function cryptoSequenceGenerator(seed1, seed2, seed3, sequenceSize) {
+    const sequence = [seed1, seed2, seed3];
+
+    const modulo = Math.pow(2, 35) - 849;
+    for (let index = 3; index < sequenceSize; index++) {
+        sequence.push((1995 * sequence[index - 1] + 1998 * sequence[index - 2] + 2001 * sequence[index - 3]) % modulo);
+    }
+
+    return sequence;
+}
+
+// generateAndDownloadFile(cryptoSequenceGenerator(101, 999, 2350, 1024));
