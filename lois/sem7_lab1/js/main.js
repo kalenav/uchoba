8a39217717d3aeff8b5a19f176ff3f26a34844c9
Konstantin Loktev <konstantin.loktev.02@mail.ru>
2023-11-01 17:07:33 +0300

    lishnee ubiraem

--- lois/sem7_lab1/js/main.js
+++ lois/sem7_lab1/js/main.js
@@ -10,6 +10,31 @@ const addSetButton = document.getElementById('add-set');
 const addPredicateButton = document.getElementById('add-predicate');
 const conclusionButton = document.getElementById('conclusion');
 
+class ViewUtils {
+    static tag({
+        name,
+        attributes = {},
+        child = null,
+        children = [],
+        eventListeners = {},
+        text = ""
+    }) {
+        const tag = document.createElement(name);
+        for (let attribute in attributes) {
+            tag.setAttribute(attribute, attributes[attribute]);
+        }
+        for (let eventName in eventListeners) {
+            tag.addEventListener(eventName, eventListeners[eventName]);
+        }
+        if (child) tag.appendChild(child);
+        children.forEach(child => tag.appendChild(child));
+        if (text) {
+            tag.append(text);
+        }
+        return tag;
+    }
+}
+
 class FuzzyEntityToTextUtils {
     static getTextOfFuzzyElement(fuzzyElement) {
         return `<${fuzzyElement[0]}, ${fuzzyElement[1].value}>`
